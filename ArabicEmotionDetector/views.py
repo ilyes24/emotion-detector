0f816c47e744990cbacaf1e30a79b813ed047074
ilyes24 <ilyes24ghomrani@gmail.com>
2020-07-21 23:02:13 +0100

    result view

--- ArabicEmotionDetector/views.py
+++ ArabicEmotionDetector/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .KNN import knn
 from .SVM import svm
 from .NaiveBayes import naive_bayes
@@ -16,10 +19,17 @@ def index(request):
 
         result = None
         if algorithm == 'KNN':
-            result = knn(text_input)
+            result = knn([text_input])
         elif algorithm == 'SVM':
-            result = svm(text_input)
+            result = svm([text_input])
         elif algorithm == 'Naive':
-            result = naive_bayes(text_input)
+            result = naive_bayes([text_input])
 
-        return render(request, 'result.html', {'result': result})
+        context = {'result': result, 'text_input': text_input, 'algorithm': algorithm}
+        return render(request, 'result.html', context)
+
+
+def result_view(request):
+    r = request
+    i = 20
+    return render(request, 'index.html')
