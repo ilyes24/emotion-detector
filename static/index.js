0d63866fc1fe9ac35fa70e5f109cb117babf92b6
ilyes24 <ilyes24ghomrani@gmail.com>
2020-07-24 05:34:47 +0100

    updating static files

--- static/index.js
+++ static/index.js
@@ -1,3 +1,6 @@
+$("<div style=\"padding-left: 0;\" class=\"padding-0 custom-control margin-right-0 custom-control-inline\">Choose an algorithm:</div>").insertBefore("ul");
+
 function classifie(){
     $('#logo').backgroundColor("red");
 }
+
